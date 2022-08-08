a38bc92f533289d6d305992809f794584f330bf1
Róbert Belanec <belanecrobert22@gmail.com>
2022-08-08 13:24:15 +0200

    removed html files, old templates and old images

--- make-html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-template="template.html"
-output="sprievodca-matfyzaka.html"
-
-# TODO spojaznit --transliterated-header-ids
-
-kramdown -o html --toc-levels "2..3"  --template $template sprievodca-matfyzaka.markdown > $output;
